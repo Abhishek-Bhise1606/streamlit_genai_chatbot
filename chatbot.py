c63227a5fda2d80fb9e9748352a749364976240e
Abhishek Bhise <abhishekbhise021@gmail.com>
2026-05-21 11:23:17 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,60 +1,21 @@
 import streamlit as st
-
-st.write(st.secrets)
-import streamlit as st
 from langchain_groq import ChatGroq
 
-# load env variables
+st.title("🤖 Chatbot")
 
+api_key = st.secrets["GROQ_API_KEY"]
 
-# streamlit page setup
-st.set_page_config(
-    page_title="Generative AI ChatBot",
-    page_icon="📥",
-    layout="centered",
-)
+st.write(api_key[:10])   # temporary test
 
-st.title("🤖CHOTU")
-
-# initiate chat history
-if "chat_history" not in st.session_state:
-    st.session_state["chat_history"] = []
-
-# show old chat messages
-for message in st.session_state.chat_history:
-    with st.chat_message(message["role"]):#with:Every thing belong to that chat bubbal
-        st.markdown(message["content"])
-
-# initialize LLM
 llm = ChatGroq(
-    groq_api_key=st.secrets["GROQ_API_KEY"],
+    api_key=api_key,
     model="llama-3.3-70b-versatile",
     temperature=0.0,
 )
-# user input
-user_prompt = st.chat_input("Ask a question")
 
-if user_prompt:# runs only user enter something
+user_input = st.chat_input("Ask something")
 
-    # show user message
-    st.chat_message("user").markdown(user_prompt)# disply user message instantly
+if user_input:
+    response = llm.invoke(user_input)
 
-    # save user message
-    st.session_state.chat_history.append({"role": "user", "content": user_prompt}
-    )
-
-    # generate response
-    # invoke :send request to model
-    response = llm.invoke(
-        input=[{"role":"system","content":"you are a helpful assistant"},*st.session_state.chat_history
-    ])#* unpack list items
-
-    assistant_response = response.content
-
-    # save assistant response
-    st.session_state.chat_history.append(
-        {"role": "assistant", "content": assistant_response}
-    )
-    # show assistant response
-    with st.chat_message("assistant"):
-        st.markdown(assistant_response)
+    st.write(response.content)
